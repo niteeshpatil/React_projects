f5f3141a3437977e9bcf91270a35a8067ecd9da4
niteeshpatil <niteeshgpatil5@gmail.com>
2022-05-05 19:26:28 +0530

    In Project_14 Redux Basics used to check funtionality on Redux

---
+++ project_14/redux-demo.js
@@ -0,0 +1,33 @@
+const redux = require("redux");
+
+// function counterReducer() {}
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+
+  return state;
+};
+
+const store = redux.createStore(counterReducer);
+
+console.log(store.getState());
+
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });
