005c34d4dad98edc87d1bffd6fbadd26d020669c
niteeshpatil <niteeshgpatil5@gmail.com>
2022-05-05 19:53:03 +0530

    In project_15 bulding webapp with using Redux

---
+++ project_15/src/store/index.js
@@ -0,0 +1,20 @@
+import { createStore } from "redux";
+
+const CounterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+
+  return state;
+};
+
+const store = createStore(CounterReducer);
+
+export default store;
