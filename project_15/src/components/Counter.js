9def6aef48eec6f33f57dcbad106533c35853069
niteeshpatil <niteeshgpatil5@gmail.com>
2022-05-18 19:03:42 +0530

    In project_15, bulit an increment and decrement funitonality

--- project_15/src/components/Counter.js
+++ project_15/src/components/Counter.js
@@ -1,12 +1,29 @@
-import classes from './Counter.module.css';
+import { useSelector, useDispatch } from "react-redux";
+import classes from "./Counter.module.css";
 
 const Counter = () => {
+  const dispatch = useDispatch();
+  // dispactch an action agenest store
+  const counter = useSelector((state) => state.counter);
+  //funtion determin which freacher we want to extreact
+  // auto changing when
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+  const derementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={derementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
