7426d27264b7999abedfa7df7093a1fb3ef482e2
niteeshpatil <niteeshgpatil5@gmail.com>
2022-05-25 18:18:52 +0530

    IN project_17, two pages are implimented using react-router-dom Route,BrowserRouter

--- project_17/src/App.js
+++ project_17/src/App.js
@@ -1,9 +1,20 @@
+import { Route } from "react-router-dom";
+import Welcome from "./components/Welcome";
+import Products from "./components/products";
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Route path="/welcome">
+        <Welcome></Welcome>
+      </Route>
+      <Route path="/products">
+        <Products></Products>
+      </Route>
     </div>
   );
 }
 
 export default App;
+
+/* diplayed only 'mainpagename/wecome' is as url */
+/*  diplayed only 'mainpagename/products' is as url */
