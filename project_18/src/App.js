9712daa09cd7461ba499f4e136ceaa01ca6a7cf1
niteeshpatil <niteeshgpatil5@gmail.com>
2022-05-29 15:33:05 +0530

    IN project_18, nested routes and layout wrapper is added to project

--- project_18/src/App.js
+++ project_18/src/App.js
@@ -1,8 +1,27 @@
+import { Route, Switch, Redirect } from "react-router-dom";
+import AllQuotes from "./pages/AllQuotes";
+import QuoteDetail from "./pages/QuoteDetail";
+import NewQuote from "./pages/NewQuotes";
+import Layout from "./components/layout/Layout";
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes"></Redirect>
+        </Route>
+        <Route path="/quotes" exact>
+          <AllQuotes></AllQuotes>
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <QuoteDetail></QuoteDetail>
+        </Route>
+        <Route path="/new-quote">
+          <NewQuote></NewQuote>
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
