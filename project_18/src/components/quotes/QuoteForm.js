a11b9a0315f737aa3fbbe9e88761c299de17cb8e
niteeshpatil <niteeshgpatil5@gmail.com>
2022-05-29 16:32:57 +0530

    In project_18, waring user by using prompt massage

--- project_18/src/components/quotes/QuoteForm.js
+++ project_18/src/components/quotes/QuoteForm.js
@@ -1,10 +1,15 @@
-import { useRef } from "react";
+import { useRef, useState, Fragment } from "react";
+import { Prompt } from "react-router-dom";
+// <Prompt>. Used to prompt the user before navigating away
+// from a page. When your application enters a state that should
+// prevent the user from navigating away ...
 
 import Card from "../UI/Card";
 import LoadingSpinner from "../UI/LoadingSpinner";
 import classes from "./QuoteForm.module.css";
 
 const QuoteForm = (props) => {
+  const [isEntered, setIsEntered] = useState(false);
   const authorInputRef = useRef();
   const textInputRef = useRef();
 
@@ -19,28 +24,48 @@ const QuoteForm = (props) => {
     props.onAddQuote({ author: enteredAuthor, text: enteredText });
   }
 
-  return (
-    <Card>
-      <form className={classes.form} onSubmit={submitFormHandler}>
-        {props.isLoading && (
-          <div className={classes.loading}>
-            <LoadingSpinner />
-          </div>
-        )}
+  const formFocuseHandler = () => {
+    setIsEntered(true);
+  };
 
-        <div className={classes.control}>
-          <label htmlFor="author">Author</label>
-          <input type="text" id="author" ref={authorInputRef} />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="text">Text</label>
-          <textarea id="text" rows="5" ref={textInputRef}></textarea>
-        </div>
-        <div className={classes.actions}>
-          <button className="btn">Add Quote</button>
-        </div>
-      </form>
-    </Card>
+  const finishHandler = () => {
+    setIsEntered(false);
+  };
+
+  return (
+    <Fragment>
+      <Prompt
+        when={isEntered}
+        message={(location) => "Are you realy want to quite the page"}
+      ></Prompt>
+      <Card>
+        <form
+          onFocus={formFocuseHandler}
+          className={classes.form}
+          onSubmit={submitFormHandler}
+        >
+          {props.isLoading && (
+            <div className={classes.loading}>
+              <LoadingSpinner />
+            </div>
+          )}
+
+          <div className={classes.control}>
+            <label htmlFor="author">Author</label>
+            <input type="text" id="author" ref={authorInputRef} />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="text">Text</label>
+            <textarea id="text" rows="5" ref={textInputRef}></textarea>
+          </div>
+          <div className={classes.actions}>
+            <button className="btn" onClick={finishHandler}>
+              Add Quote
+            </button>
+          </div>
+        </form>
+      </Card>
+    </Fragment>
   );
 };
 
--- project_18/src/pages/NewQuotes.js
+++ project_18/src/pages/NewQuotes.js
@@ -1,8 +1,13 @@
 import QuoteForm from "../components/quotes/QuoteForm";
+import { useHistory } from "react-router-dom";
+// The useHistory hook gives you access to the history instance that you may use to navigate. ...
 
 const NewQuote = () => {
+  const history = useHistory();
+
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+    history.push("/quotes");
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
