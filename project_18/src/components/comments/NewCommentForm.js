32ef21a7ca71ec64f610ab48fad03198da391fef
niteeshpatil <niteeshgpatil5@gmail.com>
2022-06-01 19:55:04 +0530

    project is succesfuly hosted on firebase

--- project_18/src/components/comments/NewCommentForm.js
+++ project_18/src/components/comments/NewCommentForm.js
@@ -14,7 +14,7 @@ const NewCommentForm = (props) => {
     if (status === "completed" && !error) {
       onAddedComment();
     }
-  }, [status, error]);
+  }, [status, error, onAddedComment]);
 
   const submitFormHandler = (event) => {
     event.preventDefault();
